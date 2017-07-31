999e4ccb32224bf0a02779f5cb4085873fff1935
badmode <jrusso7@mail.csuchico.edu>
2017-07-31 13:11:34 -0700

    7/31/17

--- stronghold/gc.py
+++ stronghold/gc.py
@@ -41,3 +41,46 @@ Sample Output
 Rosalind_0808
 60.919540
 '''
+'''import math
+from Bio import SeqIO
+def parser(name, sequence):
+    count = 0
+    for i in sequence:
+        if i == 'G':
+            count += 1
+        elif i == 'C':
+            count += 1
+        else:
+            count += 0
+    print(name)
+    print(sequence)
+    percentage = float(count/len(sequence))
+    print(percentage)
+'''
+
+def read_fasta(fp):
+    name, seq = None, []
+    for line in fp:
+        line = line.rstrip()
+        if line.startswith(">"):
+            if name: yield (name, ''.join(seq))
+            name, seq = line, []
+        else:
+            seq.append(line)
+    if name: yield (name, ''.join(seq))
+    return
+def parser(name, sequence):
+    count = 0
+    for i in sequence:
+        if i == 'G':
+            count += 1
+        elif i == 'C':
+            count += 1
+        else:
+            count += 0
+    print(name)
+    percentage = float((count/len(sequence)) * 100)
+    print(percentage, '%')
+with open('rosalind_gc.txt') as fp:
+    for name, seq in read_fasta(fp):
+        parser(name, seq)
--- stronghold/subs.py
+++ stronghold/subs.py
@@ -15,7 +15,16 @@ Sample Output
 
 2 4 10
 '''
-
-s = 'GATATATGCATATACTT'
-s2 = 'ATAT'
-print s.find(s2)
+def findall_iter(sub, string):
+    """
+    >>> text = "Allowed Hello Hollow"
+    >>> tuple(findall_iter('ll', text))
+    (1, 10, 16)
+    """
+    def next_index(length):
+        index = 0 - length
+        while True:
+            index = string.find(sub, index + length)
+            yield index
+    return iter(next_index(len(sub)).next, -1)
+print(findall_iter('ATAT','GATATATGCATATACTT'))
