a0e26bc5e206c5ada7d2dfe3b1320f6f1178f366
badmode <jrusso7@mail.csuchico.edu>
2017-08-06 11:43:37 -0700

    8/3/17

---
+++ practice/prtm_practice.py
@@ -0,0 +1,6 @@
+from Bio.SeqUtils.ProtParam import ProteinAnalysis
+
+class ProteinAnalysis:
+    def __init__(self, sequence, monoisotopic=True):
+        self.sequence = sequence
+print(molecular_weight(self))
